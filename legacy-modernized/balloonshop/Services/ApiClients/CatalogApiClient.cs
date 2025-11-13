5a71029cf6b63f5423290f4819ba53a41ea4a7a9
FarhanIqbal2003 <farhaniqbal2003@hotmail.com>
2025-11-13 23:57:03 +0500

    Create class to communicate with API

---
+++ legacy-modernized/balloonshop/Services/ApiClients/CatalogApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+//using System.Net.Http;
+//using System.Net.Http.Json;
+//using System.Threading.Tasks;
+
+
+/// <summary>
+/// Summary description for CatalogApiClient
+/// </summary>
+public class CatalogApiClient
+{
+    //private readonly HttpClient _client;
+    private readonly string _baseUrl;
+
+    public CatalogApiClient()
+    {
+        //_client = new HttpClient();
+        _baseUrl = ConfigurationManager.AppSettings["CatalogService.BaseUrl"];
+    }
+}
