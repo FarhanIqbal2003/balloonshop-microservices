a8c151e4cdf77689d643055213cb289bb1c3e199
FarhanIqbal2003 <farhaniqbal2003@hotmail.com>
2025-11-14 11:51:34 +0500

    Update libraries to support .net framework 3.5

--- legacy-modernized/balloonshop/Services/ApiClients/CatalogApiClient.cs
+++ legacy-modernized/balloonshop/Services/ApiClients/CatalogApiClient.cs
@@ -1,24 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
-//using System.Net.Http;
-//using System.Net.Http.Json;
-//using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
 
+
+/// <summary>
+/// Summary description for CatalogApiClient
+/// </summary>
 
 /// <summary>
 /// Summary description for CatalogApiClient
 /// </summary>
 public class CatalogApiClient
 {
-    //private readonly HttpClient _client;
     private readonly string _baseUrl;
 
     public CatalogApiClient()
     {
-        //_client = new HttpClient();
         _baseUrl = ConfigurationManager.AppSettings["CatalogService.BaseUrl"];
     }
+
+    public List<ProductDto> GetProducts()
+    {
+        var url = _baseUrl + "/api/catalog";
+        using (var client = new WebClient())
+        {
+            client.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+            try
+            {
+                var json = client.DownloadString(url);
+                var serializer = new JavaScriptSerializer();
+                return serializer.Deserialize<List<ProductDto>>(json);
+            }
+            catch //(Exception ex)
+            {
+                // Log exception
+                return new List<ProductDto>();
+            }
+        }
+    }
+}
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
 }
